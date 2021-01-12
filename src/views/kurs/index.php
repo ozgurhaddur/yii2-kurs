4c9df6432dd52e1f9a5203617cbce5ee091808c0
ozgurhaddur <73740709+ozgurhaddur@users.noreply.github.com>
2021-01-12 20:25:20 +0300

    Update index.php

--- src/views/kurs/index.php
+++ src/views/kurs/index.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
-/* @var $searchModel backend\modules\kurs\models\KursSearch */
+/* @var $searchModel ozgurhaddur\kurs\models\KursSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Kurs';
