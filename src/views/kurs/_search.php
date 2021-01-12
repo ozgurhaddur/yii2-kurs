f443cc6bb1b233bca80fcee61284a9271742cd12
ozgurhaddur <73740709+ozgurhaddur@users.noreply.github.com>
2021-01-12 20:23:51 +0300

    Update _search.php

--- src/views/kurs/_search.php
+++ src/views/kurs/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model backend\modules\kurs\models\KursSearch */
+/* @var $model ozgurhaddur\kurs\models\KursSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
