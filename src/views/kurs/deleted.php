37e2f35ceaebaf84593c0cff0466cc9279af2436
ozgurhaddur <73740709+ozgurhaddur@users.noreply.github.com>
2021-01-12 20:24:59 +0300

    Update deleted.php

--- src/views/kurs/deleted.php
+++ src/views/kurs/deleted.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
-/* @var $searchModel backend\modules\kurs\models\DeletedSearch */
+/* @var $searchModel ozgurhaddur\kurs\models\DeletedSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Deleted';
